70ce775d62a310c0ddaf39ee2dfdd3d304a1b832
Architaviswanathan <127740093+Architaviswanathan@users.noreply.github.com>
2025-02-06 10:21:43 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u archita392 -p 12345678910
-    docker tag test archita392/task_2
-    docker push archita392/task_2
+    docker tag test archita392/task_3
+    docker push archita392/task_3
